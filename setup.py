ce7e86c94d3a2b9306486a2070cf5b5f52698629
Essam Aly <essam.aly@amd.com>
2024-04-22 12:53:22 -0700

    Setup.py changed changed to build only one time via invoking the existing CMakeLists.txt

--- setup.py
+++ setup.py
@@ -18,62 +18,21 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
  
-from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension
-from setuptools.command.install import install
 import subprocess
 import os
-import site
-from glob import glob
+from setuptools import setup, find_packages
 
-ROCM_PATH = '/opt/rocm'
-if "ROCM_PATH" in os.environ:
-    ROCM_PATH = os.environ.get('ROCM_PATH')
-print("\nROCm PATH set to -- " + ROCM_PATH + "\n")
+# Call CMake to configure and build the project
+build_dir = os.path.join(os.getcwd(), 'build')
+os.makedirs(build_dir, exist_ok=True)
+subprocess.check_call(['cmake', '-B' + build_dir, '-H' + os.getcwd()])
 
-# This block is NOT HARDCODED, it is relative to the environment os.environ.get('ROCM_PATH') if found
-UTILS_PATH = ROCM_PATH + '/share/rocdecode/utils/'
-UTILS_DEC_PATH = ROCM_PATH + '/share/rocdecode/utils/rocvideodecode/'
-ROC_DEC_PATH = ROCM_PATH + '/include/rocdecode/'
-ROCM_H_PATH = ROCM_PATH + '/include/'
-os.environ["CC"] = ROCM_PATH + '/llvm/bin/clang++'
- 
-# Custom install to run cmake before installation
-class CustomInstall(install):
-    def run(self):
-        self.build_and_install()
-        install.run(self)
+# Invoke cmake --build to build the project
+subprocess.check_call(['cmake', '--build', build_dir, '--config', 'Release'])
 
-    def build_and_install(self):
-        # Set the build directory relative to the setup.py file
-        build_temp=os.path.join(os.path.dirname(os.path.abspath(__file__)),"build")
+# Install the built binaries
+subprocess.check_call(['cmake', '--install', build_dir])
 
-        #path to python pacakges like pybind11
-        python_site_package_path = site.getsitepackages()
-        python_site_package_path = ';'.join(python_site_package_path)
-
-        # Run cmake
-        cmake_args=["cmake", ".", "-B"+build_temp, "-Dpybind11_DIR="+python_site_package_path]
-        subprocess.check_call(cmake_args,cwd=os.getcwd())
-
-        # Run cmake --build to compile
-        subprocess.check_call(["cmake","--build",build_temp,"--target","install"],cwd=build_temp)
-
-# Define the extension module
-ext_modules = [
-    Pybind11Extension(
-        'rocPyDecode', 
-        sources=sorted(glob("src/*.cpp"))+[UTILS_DEC_PATH+"roc_video_dec.cpp"],
-        include_dirs=[ROCM_H_PATH, ROC_DEC_PATH, UTILS_PATH, UTILS_DEC_PATH, 'src' ],
-        extra_compile_args=['-D__HIP_PLATFORM_AMD__','-Wno-unused-private-field','-Wno-ignored-optimization-argument', '-Wno-missing-braces', '-Wno-sign-compare', '-Wno-sign-compare','-Wno-reorder','-Wno-int-in-bool-context', '-Wno-unused-variable'],
-        library_dirs=[ROCM_PATH+'/lib/','/usr/local/lib/','/usr/lib/x86_64-linux-gnu/'],
-        libraries=['rocdecode','avcodec','avformat','avutil'],
-        runtime_library_dirs=[],
-        extra_link_args=[],
-    )
-]
-
-# Setup
 setup(
       name='rocPyDecode',
       description='AMD ROCm Video Decoder Library',
@@ -82,9 +41,7 @@ setup(
       author='AMD',
       license='MIT License',
       setup_requires=["pybind11"],
-      ext_modules=ext_modules,
-      cmdclass={'install':CustomInstall},
-      packages=['pyRocVideoDecode'],
+      packages=find_packages(),
       package_dir={'pyRocVideoDecode':'pyRocVideoDecode'},
       package_data={"pyRocVideoDecode":["__init__.pyi"]},
       )
