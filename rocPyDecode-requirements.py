737ccb5c6112cc3e77fa87a4b7dfa50a765a9a3a
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-06-27 13:16:36 -0600

    Update install

--- rocPyDecode-requirements.py
+++ rocPyDecode-requirements.py
@@ -168,36 +168,36 @@ if(docker_image == 'YES'):
 else:
     ERROR_CHECK(os.system('pip3 install pybind11'))
 
-# if "Ubuntu" in platfromInfo:
+if "Ubuntu" in platfromInfo:
 # core debian packages
-if rocdecodeInstall == 'ON':
-    for i in range(len(coreDebianPackages)):
-        ERROR_CHECK(os.system('sudo '+linuxFlag+' '+linuxSystemInstall +
-                ' '+linuxSystemInstall_check+' install '+ coreDebianPackages[i]))
-# dlpack
-# TODO: make this dynamic allowing to change the ver# via parameter
-ERROR_CHECK(os.system('wget http://archive.ubuntu.com/ubuntu/pool/universe/d/dlpack/libdlpack-dev_0.6-1_amd64.deb'))
-# install needed z package
-ERROR_CHECK(os.system(linuxSystemInstall+' install zstd'))
-# Extract files from the archive
-ERROR_CHECK(os.system('ar x libdlpack-dev_0.6-1_amd64.deb'))
-# Uncompress zstd files an re-compress them using xz
-ERROR_CHECK(os.system('zstd -d < control.tar.zst | xz > control.tar.xz'))
-ERROR_CHECK(os.system('zstd -d < data.tar.zst | xz > data.tar.xz'))
-# Re-create the Debian package in /tmp/
-ERROR_CHECK(os.system('ar -m -c -a sdsd /tmp/libdlpack-dev_0.6-1_amd64.deb debian-binary control.tar.xz data.tar.xz'))
-# Clean up
-ERROR_CHECK(os.system('rm debian-binary control.tar.xz data.tar.xz control.tar.zst data.tar.zst'))
-# install the deb now
-ERROR_CHECK(os.system('dpkg -i /tmp/libdlpack-dev_0.6-1_amd64.deb'))
+    if rocdecodeInstall == 'ON':
+        for i in range(len(coreDebianPackages)):
+            ERROR_CHECK(os.system('sudo '+linuxFlag+' '+linuxSystemInstall +
+                    ' '+linuxSystemInstall_check+' install '+ coreDebianPackages[i]))
+    # dlpack
+    # TODO: make this dynamic allowing to change the ver# via parameter
+    ERROR_CHECK(os.system('wget http://archive.ubuntu.com/ubuntu/pool/universe/d/dlpack/libdlpack-dev_0.6-1_amd64.deb'))
+    # install needed z package
+    ERROR_CHECK(os.system(linuxSystemInstall+' install zstd'))
+    # Extract files from the archive
+    ERROR_CHECK(os.system('ar x libdlpack-dev_0.6-1_amd64.deb'))
+    # Uncompress zstd files an re-compress them using xz
+    ERROR_CHECK(os.system('zstd -d < control.tar.zst | xz > control.tar.xz'))
+    ERROR_CHECK(os.system('zstd -d < data.tar.zst | xz > data.tar.xz'))
+    # Re-create the Debian package in /tmp/
+    ERROR_CHECK(os.system('ar -m -c -a sdsd /tmp/libdlpack-dev_0.6-1_amd64.deb debian-binary control.tar.xz data.tar.xz'))
+    # Clean up
+    ERROR_CHECK(os.system('rm debian-binary control.tar.xz data.tar.xz control.tar.zst data.tar.zst'))
+    # install the deb now
+    ERROR_CHECK(os.system('dpkg -i /tmp/libdlpack-dev_0.6-1_amd64.deb'))
 
-# elif "redhat" in platfromInfo:
-#     # core RPM packages
-#     if rocdecodeInstall == 'ON':
-#         for i in range(len(coreRPMPackages)):
-#             ERROR_CHECK(os.system('sudo '+linuxFlag+' '+linuxSystemInstall +
-#                     ' '+linuxSystemInstall_check+' install '+ coreRPMPackages[i]))
-#     # dlpack
-#     # TODO
+else:
+    # core RPM packages
+    if rocdecodeInstall == 'ON':
+        for i in range(len(coreRPMPackages)):
+            ERROR_CHECK(os.system('sudo '+linuxFlag+' '+linuxSystemInstall +
+                    ' '+linuxSystemInstall_check+' install '+ coreRPMPackages[i]))
+    # dlpack
+    # TODO
 
 print("\rocPyDecode Dependencies Installed with rocPyDecode-setup.py V-"+__version__+"\n")
