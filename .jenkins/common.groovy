38c092e516ed21d3f67fe85855aaae6a25625a92
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-12-12 11:22:48 -0700

    For CI, change dir to project build prefix before building rocDecode

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -9,7 +9,9 @@ def runCompileCommand(platform, project, jobName, boolean debug=false, boolean s
     
     def command = """#!/usr/bin/env bash
                 set -ex
-
+                
+                cd ${project.paths.project_build_prefix}
+                
                 echo Build rocDecode
                 rm -rf rocDecode
                 git clone http://github.com/ROCm/rocDecode.git
@@ -23,7 +25,6 @@ def runCompileCommand(platform, project, jobName, boolean debug=false, boolean s
                 cd ../..
 
                 echo Build rocPyDecode - ${buildTypeDir}
-                cd ${project.paths.project_build_prefix}
                 
                 wget https://github.com/dmlc/dlpack/archive/refs/tags/v0.6.tar.gz
                 tar -xvf v0.6.tar.gz
