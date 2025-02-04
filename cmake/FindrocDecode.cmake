21449b367624eda4ff88d6890af74a47c100022c
kiritigowda <kiritigowda@gmail.com>
2025-02-04 14:26:07 -0800

    FindrocDecode - Cleanup

--- cmake/FindrocDecode.cmake
+++ cmake/FindrocDecode.cmake
@@ -96,8 +96,8 @@ if(rocDecode_FOUND)
         string(REGEX MATCH "ROCDECODE_MICRO_VERSION ([0-9]*)" _ ${ROCDECODE_VERSION_FILE})
         set(ROCDECODE_MICRO_VERSION ${CMAKE_MATCH_1} CACHE INTERNAL "")
         message("-- ${White}Using rocDecode --  \n\tLibraries:${rocDecode_LIBRARIES} 
-                                                \n\tIncludes:${rocDecode_INCLUDE_DIRS}
-                                                \n\tVersion:${ROCDECODE_MAJOR_VERSION}.${ROCDECODE_MINOR_VERSION}.${ROCDECODE_MICRO_VERSION}${ColourReset}")
+                                                \tIncludes:${rocDecode_INCLUDE_DIRS}
+                                                \tVersion:${ROCDECODE_MAJOR_VERSION}.${ROCDECODE_MINOR_VERSION}.${ROCDECODE_MICRO_VERSION}${ColourReset}")
     else()
         set(ROCDECODE_MAJOR_VERSION 0)
         set(ROCDECODE_MINOR_VERSION 0)
