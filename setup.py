63160a2066c16b0416ff0bd6ec60cda7f770a521
kiritigowda <kiritigowda@gmail.com>
2025-02-06 17:14:14 -0800

    Pip package - name updates

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ subprocess.check_call(['cmake', '--build', build_dir, '--config', 'Release', '--
 subprocess.check_call(['cmake', '--install', build_dir],cwd=os.getcwd())
 
 setup(
-    name='amd_rocpydecode',
+    name='amd_rocpydecode_instinct',
     description='AMD ROCm Video Decoder Library',
     url='https://github.com/ROCm/rocPyDecode',
     version='0.4.0' + '.' + get_rocm_rev(),
